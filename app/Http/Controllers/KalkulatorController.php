f075062723a43e58dabe177cb36c73035535b829
mayus123 <60176333+mayus123@users.noreply.github.com>
2020-09-15 14:45:49 +0700

    Controller Kalkulator Rumus

--- app/Http/Controllers/KalkulatorController.php
+++ app/Http/Controllers/KalkulatorController.php
@@ -6,25 +6,42 @@ use Illuminate\Http\Request;
 
 class KalkulatorController extends Controller
 {
-    public function calculate(Request $request)
+    // Menghitung Tabung
+    public function calculate_tabung(Request $request)
     {
-        $operasi = $request->input('operasi');
-        $bil_pertama = $request->input('bil_1');
-        $bil_kedua = $request->input('bil_2');
-        $result = 0;
+        $r = $request->input('jari_jari');
+        $t = $request->input('tinggi');
 
-        if ($operasi == "tambah") {
-           $result = $bil_pertama + $bil_kedua;
-        } else if ($operasi == "kurang") {
-            $result = $bil_pertama - $bil_kedua;
-        } else if ($operasi == "kali") {
-            $result = $bil_pertama * $bil_kedua;
-        } else if ($operasi == "bagi") {
-            $result = $bil_pertama / $bil_kedua;
-        } else {
-            $result = 0;
-        }
+        $total_luas = $r * $r;
 
-        return redirect('/')->with('info', 'Hasilnya adalah : ' .$result);
+        $result = $total_luas * $t;
+        
+
+        return redirect('/')->with('info', 'Hasil Volume Tabung adalah : ' .$result);
+    }
+
+    public function calculate_bola(Request $request)
+    {
+        $empat_per_tiga = $request->input('empatpertiga') ?: 1.33;
+        $ro = $request->input('ro') ?: 3.14;
+        $r = $request->input('jari_jari');
+
+        $result = $empat_per_tiga * $ro * $r * $r * $r;
+
+        return redirect('/')->with('info_hasil_volume_bola', 'Hasil Volume Bola adalah : ' .$result);
+    }
+
+    public function calculate_kerucut(Request $request)
+    {
+        $satu_per_tiga = $request->input('satupertiga') ?: 0.33;
+        $ro = $request->input('ro') ?: 3.14;
+        $r = $request->input('jari_jari');
+        $t = $request->input('tinggi');
+
+        $total_luas = $r * $r;
+
+        $result = $satu_per_tiga * $ro * $total_luas * $t;
+
+        return redirect('/')->with('hasil_volume_kerucut', 'Hasil Volume Bola adalah : ' .$result);
     }
 }
